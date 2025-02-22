8449714266697c290e0d023fa85c375047876141
jessicaaurel <jessicaaureliaw@gmail.com>
2025-02-22 18:36:55 +0700

    minor changes in features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import { Link as RouterLink } from 'react-router-dom';
 import { AiOutlineArrowRight } from 'react-icons/ai';
 import { AiOutlineGlobal } from "react-icons/ai";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
