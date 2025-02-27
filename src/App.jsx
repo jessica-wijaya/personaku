0c9bf7741db0cd1b3c49897e036f95c5ce99d186
jessicaaurel <jessicaaureliaw@gmail.com>
2025-02-28 00:13:02 +1100

    added whatsapp button

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Video from './components/Video'
-import Features from './components/Features'
-import Manfaat from './components/Manfaat'
-import Demo from './components/Demo'
-import Harga from './components/Harga'
-import Footer from './components/Footer'
-import Testimonial from './components/Testimonial'
-import FAQ from './components/FAQ'
-import Contact from './components/Contact'
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Video from "./components/Video";
+import Features from "./components/Features";
+import Manfaat from "./components/Manfaat";
+import Demo from "./components/Demo";
+import Harga from "./components/Harga";
+import Footer from "./components/Footer";
+import Testimonial from "./components/Testimonial";
+import FAQ from "./components/FAQ";
+import Contact from "./components/Contact";
+import Whatsapp from "./components/Whatsapp";
 const App = () => {
   return (
     <Router>
@@ -20,17 +20,21 @@ const App = () => {
 
         {/* Main page content */}
         <Routes>
-          <Route path="/personaku/" element={
-            <div>
-              <Hero />
-              <Video />
-              <Features />
-              <Manfaat />
-              <Demo />
-              <Harga />
-              <Testimonial />
-            </div>
-          } />
+          <Route
+            path="/personaku/"
+            element={
+              <div>
+                <Hero />
+                <Video />
+                <Features />
+                <Manfaat />
+                <Demo />
+                <Harga />
+                <Testimonial />
+                <Whatsapp />
+              </div>
+            }
+          />
 
           <Route path="/personaku/faq" element={<FAQ />} />
           <Route path="/personaku/contact" element={<Contact />} />
@@ -39,7 +43,7 @@ const App = () => {
         <Footer />
       </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Whatsapp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import icon from "../assets/whatsapp.png";
+
+const Whatsapp = () => {
+  return (
+    <div className="fixed right-0 top-1/2 transform -translate-y-1/2 bg-white w-[93px] h-[222px] flex flex-col items-center rounded-[18px]">
+      <button className="w-full h-full cursor-pointer hover:underline flex flex-col items-center">
+        <img src={icon} className="w-[54px] h-auto mt-4" alt="WhatsApp Icon" />
+        <p className="rotate-270 mt-15 mb-4">WhatsApp</p>
+      </button>
+    </div>
+  );
+};
+
+export default Whatsapp;
