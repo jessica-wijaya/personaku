9f8900aaf09378c2838ddc8df08d04fa72477700
jessicaaurel <jessicaaureliaw@gmail.com>
2025-03-08 21:59:52 +1100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,14 +18,14 @@ const Navbar = () => {
     setNav(false);
   };
 
-  // show/hide navbar
   useEffect(() => {
     const handleScroll = () => {
+      if (nav) {
+        setNav(false);
+      }
       if (window.scrollY < lastScrollY.current) {
-        // scroll up: show navbar
         setShowNavbar(true);
       } else {
-        // scroll down: hide navbar
         setShowNavbar(false);
       }
       lastScrollY.current = window.scrollY;
@@ -33,130 +33,149 @@ const Navbar = () => {
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  }, [nav]);
 
   return (
-    <nav
-      className={`fixed top-0 left-0 w-full z-50 bg-white flex items-center justify-between h-[87px] px-5 md:px-10 border-b border-[#DEE8F9] transition-transform duration-500 ${
-        showNavbar ? "translate-y-0" : "-translate-y-full"
-      }`}
-    >
-      {/* Logo */}
-      <img
-        className="h-[90px] p-[27px_2px_36.254px_10px] ml-[5px] mt-[15px] flex-shrink-0"
-        src={logo}
-        alt="logo"
-      />
-
-      {/* Desktop Menu */}
-      <ul className="hidden md:flex space-x-6">
-        <li className="cursor-pointer hover:text-[#1658F9]">
-          <RouterLink to="/personaku/" smooth={true} duration={500}>
-            Home
-          </RouterLink>
-        </li>
-        <li className="cursor-pointer hover:text-[#1658F9]">
-          <HashLink smooth to="/personaku/#harga" duration={500}>
-            Harga
-          </HashLink>
-        </li>
-        <li className="cursor-pointer hover:text-[#1658F9]">
-          <HashLink smooth to="/personaku/#demo" duration={500}>
-            Demo
-          </HashLink>
-        </li>
-        <li className="cursor-pointer hover:text-[#1658F9]">
-          <HashLink smooth to="/personaku/#testimoni" duration={500}>
-            Testimoni
-          </HashLink>
-        </li>
-        <li className="cursor-pointer hover:text-[#1658F9]">
-          <RouterLink to="/personaku/faq" smooth={true} duration={500}>
-            FAQ
-          </RouterLink>
-        </li>
-      </ul>
-
-      {/* Mobile Menu Icon */}
-      <div className="md:hidden" onClick={handleNav}>
-        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
-      </div>
-
-      {/* Mobile Dropdown Menu */}
-      <div
-        className={`fixed top-0 left-0 w-1/2 h-full bg-white z-50 transition-transform ${
-          nav ? "translate-x-0" : "-translate-x-full"
+    <>
+      <nav
+        className={`fixed top-0 left-0 w-full z-50 bg-white flex items-center justify-between h-[87px] px-5 md:px-10 border-b border-[#DEE8F9] transition-transform duration-500 ${
+          showNavbar ? "translate-y-0" : "-translate-y-full"
         }`}
       >
-        <img className="h-[60px] p-5" src={logo} alt="logo" />
-        <ul className="space-y-5 text-lg pl-5 my-4">
+        {/* Logo */}
+        <img
+          className="h-[90px] p-[27px_2px_36.254px_10px] ml-[5px] mt-[15px] flex-shrink-0"
+          src={logo}
+          alt="logo"
+        />
+
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex space-x-6">
           <li className="cursor-pointer hover:text-[#1658F9]">
-            <RouterLink
-              smooth
-              to="/personaku/"
-              duration={500}
-              onClick={handleMenuItemClick}
-            >
+            <RouterLink to="/personaku/" smooth={true} duration={500}>
               Home
             </RouterLink>
           </li>
           <li className="cursor-pointer hover:text-[#1658F9]">
-            <HashLink
-              smooth
-              to="/personaku/#harga"
-              duration={500}
-              onClick={handleMenuItemClick}
-            >
+            <HashLink smooth to="/personaku/#harga" duration={500}>
               Harga
             </HashLink>
           </li>
           <li className="cursor-pointer hover:text-[#1658F9]">
-            <HashLink
-              smooth
-              to="/personaku/#demo"
-              duration={500}
-              onClick={handleMenuItemClick}
-            >
+            <HashLink smooth to="/personaku/#demo" duration={500}>
               Demo
             </HashLink>
           </li>
           <li className="cursor-pointer hover:text-[#1658F9]">
-            <HashLink
-              smooth
-              to="/personaku/#testimoni"
-              duration={500}
-              onClick={handleMenuItemClick}
-            >
+            <HashLink smooth to="/personaku/#testimoni" duration={500}>
               Testimoni
             </HashLink>
           </li>
           <li className="cursor-pointer hover:text-[#1658F9]">
-            <RouterLink
-              to="/personaku/faq"
-              smooth={true}
-              duration={500}
-              onClick={handleMenuItemClick}
-            >
+            <RouterLink to="/personaku/faq" smooth={true} duration={500}>
               FAQ
             </RouterLink>
           </li>
-          <li>Daftar</li>
-          <li>Masuk</li>
         </ul>
-      </div>
 
-      {/* Buttons (Desktop Only) */}
-      <div className="hidden md:flex space-x-4 mr-[5px]">
-        <Button variant="secondary" size="navbar">
-          <RouterLink to="/personaku/daftar" smooth={true} duration={500}>
-            Daftar
-          </RouterLink>
-        </Button>
-        <Button variant="primary" size="navbar">
-          Masuk
-        </Button>
-      </div>
-    </nav>
+        {/* Mobile Menu Icon */}
+        <div className="md:hidden" onClick={handleNav}>
+          {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+        </div>
+
+        {/* Buttons (Desktop Only) */}
+        <div className="hidden md:flex space-x-4 mr-[5px]">
+          <Button variant="secondary" size="navbar">
+            <RouterLink to="/personaku/daftar" smooth={true} duration={500}>
+              Daftar
+            </RouterLink>
+          </Button>
+          <Button variant="primary" size="navbar">
+            Masuk
+          </Button>
+        </div>
+
+        {/* Mobile Dropdown Menu */}
+        <div
+          className={`md:hidden fixed top-[87px] left-0 w-full z-50 bg-white shadow-md transition-all duration-300 ease-in-out transform ${
+            nav ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-2"
+          }`}
+        >
+          <ul className="flex flex-col p-4">
+            <li className="py-2 border-b border-[#DEE8F9]">
+              <RouterLink
+                to="/personaku/"
+                smooth={true}
+                duration={500}
+                onClick={handleMenuItemClick}
+              >
+                Home
+              </RouterLink>
+            </li>
+            <li className="py-2 border-b border-[#DEE8F9]">
+              <HashLink
+                smooth
+                to="/personaku/#harga"
+                duration={500}
+                onClick={handleMenuItemClick}
+              >
+                Harga
+              </HashLink>
+            </li>
+            <li className="py-2 border-b border-[#DEE8F9]">
+              <HashLink
+                smooth
+                to="/personaku/#demo"
+                duration={500}
+                onClick={handleMenuItemClick}
+              >
+                Demo
+              </HashLink>
+            </li>
+            <li className="py-2 border-b border-[#DEE8F9]">
+              <HashLink
+                smooth
+                to="/personaku/#testimoni"
+                duration={500}
+                onClick={handleMenuItemClick}
+              >
+                Testimoni
+              </HashLink>
+            </li>
+            <li className="py-2 border-b border-[#DEE8F9]">
+              <RouterLink
+                to="/personaku/faq"
+                smooth={true}
+                duration={500}
+                onClick={handleMenuItemClick}
+              >
+                FAQ
+              </RouterLink>
+            </li>
+            <li className="py-2 border-b border-[#DEE8F9]">
+              <RouterLink
+                to="/personaku/daftar"
+                smooth={true}
+                duration={500}
+                onClick={handleMenuItemClick}
+              >
+                Daftar
+              </RouterLink>
+            </li>
+            <li className="py-2">
+              <RouterLink
+                to="/personaku/masuk"
+                smooth={true}
+                duration={500}
+                onClick={handleMenuItemClick}
+              >
+                Masuk
+              </RouterLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
   );
 };
 
